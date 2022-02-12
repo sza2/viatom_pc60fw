f0a0ceed974f98d27372647cd26f756f9171e233
Tomek Szczęsny <mctom@tlen.pl>
2022-02-12 18:55:40 +0100

    Code formatting; Reports battery level.

--- pc60fw.py
+++ pc60fw.py
@@ -15,38 +15,49 @@ class MyDelegate(bluepy.btle.DefaultDelegate):
         while(True):
             if(len(stream) == 0):
                 break
+
             # search for sync sequence
             idx = stream.find(b'\xaa\x55')
+
             # gather more bytes if the sync sequence not found
             if(idx < 0):
                 break
+
             # check if there are enough bytes to read the message length
             # otherwise skip and gather more bytes
-            if(len(stream) >= idx + 4):
-                length = stream[idx + 3]
-                # check whether all the bytes of the message available
-                # otherwise skip and gather more bytes
-                if(len(stream) >= idx + 4 + length):
-                    # remove the bytes from the stream prior sync
-                    # (if any - as this should not happen except in case of the firs message)
-                    del stream[0 : idx]
-                    # copy the whole message 
-                    message = stream[0 : idx + 4 + length]
-                    # the last byte of the message is a CRC8/MAXIM 
-                    # the CRC sum for the whole message (including the CRC) must be 0
-                    if(libscrc.maxim8(message) != 0):
-                        print("CRC error")
-                    # remove the sync bytes and the CRC
-                    message =  message[2 : idx + 3 + length]
-                    # remove the processed bytes from the stream
-                    del stream[0 : idx + 4 + length]
-                    # messages with 0x08 on the second spot contains values appear on the OLED display
-                    if(message[2] == 0x01):
-                        print("SpO2: %d PR: %d PI: %1.1f" % (message[3], message[4], message[6] / 10))
-                else:
-                    break
-            else:
+            if(len(stream) < idx + 4):
                 break
+
+            length = stream[idx + 3]
+            # check whether all the bytes of the message available
+            # otherwise skip and gather more bytes
+            if(len(stream) < idx + 4 + length):
+                break
+
+            # remove the bytes from the stream prior sync
+            # (if any - as this should not happen except in case of the firs message)
+            del stream[0 : idx]
+
+            # copy the whole message 
+            message = stream[0 : idx + 4 + length]
+
+            # the last byte of the message is a CRC8/MAXIM 
+            # the CRC sum for the whole message (including the CRC) must be 0
+            if(libscrc.maxim8(message) != 0):
+                print("CRC error")
+
+            # remove the sync bytes and the CRC
+            message =  message[2 : idx + 3 + length]
+
+            # remove the processed bytes from the stream
+            del stream[0 : idx + 4 + length]
+
+            # messages with 0x08 on the second spot contains values appear on the OLED display
+            if(message[2] == 0x01):
+                print("SpO2: %d PR: %d PI: %1.1f" % (message[3], message[4], message[6] / 10))
+
+            if(message[2] == 0x03):
+                print("Battery Level: %d/3" % (message[3]))
 
 pulseoximeter = bluepy.btle.Peripheral(sys.argv[1], "random")
 
